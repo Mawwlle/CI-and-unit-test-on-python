1636360383288ef59a60f32cce47301563ae8f4a
Dunaev Maksim <dunaev.maksim.03@bk.ru>
2021-02-09 22:40:52 +0300

    Added Circle operations

--- main.py
+++ main.py
@@ -17,6 +17,22 @@ class Calculator:
         return self.a - self.b
 
 
+class Circle:
+    pi = 3.1415
+
+    def __init__(self, diameter):
+        self.diameter = diameter
+
+    def radius(self):
+        return self.diameter / 2
+
+    def line_length(self):
+        return self.pi * self.diameter
+
+    def square(self):
+        return self.pi * self.radius() * self.radius()
+
+
 def array_sum(args):
     try:
         summa = 0
@@ -31,14 +47,20 @@ def array_sum(args):
 def main():
     a = 4
     b = 7
-    array = [3, 4, "3"]
+    diameter = 5
+    array = [3, 4, 9]
     print(array)
+    circle = Circle(diameter)
     calculator = Calculator(a, b)
     print(str(a) + '+' + str(b) + ' =', calculator.sum())
     print(str(a) + '-' + str(b) + ' =', calculator.sub())
     print(str(a) + '*' + str(b) + ' =', calculator.mul())
     print(str(a) + '/' + str(b) + ' =', calculator.div())
     print("Sum of array :" + str(array) + " = " + str(array_sum(array)))
+    print("Operations with circle:")
+    print("Diameter = " + str(diameter) + " -> line length = " + str(circle.line_length()))
+    print("\t\t\t\t\t radius = " + str(circle.radius()))
+    print("\t\t\t\t\t square = " + str(circle.square()))
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Calculator, array_sum
+from main import Calculator, array_sum, Circle
 
 
 class TestCalc(unittest.TestCase):
@@ -27,6 +27,23 @@ class TestArraySum(unittest.TestCase):
     def test_sum_array(self):
         self.assertEqual(array_sum([13, 2, 4, 5, 6]), 13 + 2 + 4 + 5 + 6)
         self.assertEqual(array_sum([13, "test", 32, 4]), 0)
+
+
+class TestCircleOperations(unittest.TestCase):
+
+    def setUp(self):
+        self.diameter = 6
+        self.circle = Circle(self.diameter)
+
+    def test_radius(self):
+        self.assertEqual(self.circle.radius(), self.diameter / 2)
+
+    def test_square(self):
+        self.assertEqual(round(self.circle.square()), round(3.1415 * (self.diameter * self.diameter)/4))
+        self.assertEqual(self.circle.square(), 3.1415 * self.circle.radius() * self.circle.radius())
+
+    def test_line_length(self):
+        self.assertEqual(self.circle.line_length(), 3.1415 * self.diameter)
 
 
 if __name__ == '__main__':
